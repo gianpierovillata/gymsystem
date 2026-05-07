94f8d5303bb05d3f25c3d2fdaa60e1d4074b41df
Piero <ironpiero1@gmail.com>
2026-05-07 10:59:17 -0300

    agregado cliente, migraciones de usuario , agregar al admin usuarios

--- gymsite/usuarios/models.py
+++ gymsite/usuarios/models.py
@@ -1,3 +1,13 @@
 from django.db import models
 
 # Create your models here.
+from django.db import models
+
+class Usuario(models.Model):
+    nombre = models.CharField(max_length=100)
+    email = models.EmailField(unique=True)
+    password = models.CharField(max_length=100)
+    role = models.CharField(max_length=50)
+
+    def __str__(self):
+        return self.nombre
