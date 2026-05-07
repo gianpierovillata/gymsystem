d5933af31e277bc1dfe4722777ec2e16aa5f0a7e
Piero <ironpiero1@gmail.com>
2026-05-07 17:40:03 -0300

    mostrar ejercicios desdela bd en la vista

--- gymsite/ejercicios/views.py
+++ gymsite/ejercicios/views.py
@@ -1,8 +1,10 @@
 from django.http import HttpResponse
 from django.template import loader
+from .models import Ejercicio
 # Create your views here.
 
 
 def index(request):
+    ejercicios = Ejercicio.objects.all().values()
     template =loader.get_template('ejercicios.html')    
-    return HttpResponse(template.render())
+    return HttpResponse(template.render({'ejercicios': ejercicios}))
