d65f69567f1fe5d988d5cf1e073d26b423a3065f
Piero <ironpiero1@gmail.com>
2026-05-06 22:21:40 -0300

    agregado usuarios modulo y pagina

--- gymsite/usuarios/views.py
+++ gymsite/usuarios/views.py
@@ -1,10 +1,12 @@
 
 from django.http import HttpResponse
+from django.template import loader
 # Create your views here.
 
 
 def bienvenida(request):
-    return HttpResponse("Bienvenidos usuarios")
+    template = loader.get_template('bienvenida.html')
+    return HttpResponse(template.render())
 
 def listado_usuarios(request):
     usuarios = ["Usuario 1", "Usuario 2", "Usuario 3"]
