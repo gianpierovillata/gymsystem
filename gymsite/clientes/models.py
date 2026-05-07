cc2fca01d239ae43e4c52ad6f921f990045d86e8
Piero <ironpiero1@gmail.com>
2026-05-07 11:07:57 -0300

    agregado clientes al admin , migraciones, etc

--- gymsite/clientes/models.py
+++ gymsite/clientes/models.py
@@ -1,3 +1,18 @@
 from django.db import models
+from usuarios.models import  Usuario
 
 # Create your models here.
+
+class Cliente(models.Model):
+    nombre = models.CharField(max_length=100)
+    apellido = models.CharField(max_length=100)    
+    telefono = models.CharField(max_length=20, blank=True, null=True)
+    fecha_nacimiento = models.DateField(blank=True, null=True)
+    edad = models.IntegerField(blank=True, null=True)
+    peso = models.DecimalField(max_digits=5, decimal_places=2, blank=True, null=True)
+    altura = models.DecimalField(max_digits=5, decimal_places=2, blank=True, null=True)
+
+    Usuario = models.ForeignKey(Usuario, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return f"{self.nombre} {self.apellido}"
