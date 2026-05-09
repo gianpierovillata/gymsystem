12254b02f052af4d615acdf63f86ebcaa94b98ed
Piero <ironpiero1@gmail.com>
2026-05-09 17:54:17 -0300

    ajuste de ejercicios en rutinas,poder ver los ejercicios, creada tabla de rutinas

--- gymsite/rutinas/views.py
+++ gymsite/rutinas/views.py
@@ -6,6 +6,6 @@ from .models import Rutina
 
 # Create your views here.
 def index(request):
-    rutinas = Rutina.objects.all().values()
+    rutinas = Rutina.objects.select_related('usuario').prefetch_related('ejercicios__ejercicio')
     template = loader.get_template('rutinas.html')    
     return HttpResponse(template.render({'rutinas': rutinas}))
